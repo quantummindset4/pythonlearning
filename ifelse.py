d39d4543ebca4e60d22c9f4d2deaaea6b8dcfe2b
quantummindset4 <147822552+quantummindset4@users.noreply.github.com>
2024-03-22 15:14:32 +0530

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+# Determine if a student passed or failed based on their exam score
+exam_score = 75
+
+if exam_score >= 70:
+    print("Congratulations! You passed the exam.")
+else:
+    print("Unfortunately, you failed the exam. Better luck next time.")
