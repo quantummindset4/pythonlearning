314a7892cc6bf61ffdba8544e8dc7c208f9a9623
quantummindset4 <147822552+quantummindset4@users.noreply.github.com>
2024-03-22 15:06:00 +0530

    Create integers.py
    integers

---
+++ integers.py
@@ -0,0 +1,32 @@
+# Basic arithmetic operations with integers
+num1 = 10
+num2 = 5
+
+# Addition
+result_addition = num1 + num2
+
+# Subtraction
+result_subtraction = num1 - num2
+
+# Multiplication
+result_multiplication = num1 * num2
+
+# Division
+result_division = num1 / num2
+
+# Modulus
+result_modulus = num1 % num2
+
+# Floor division
+result_floor_division = num1 // num2
+
+# Exponentiation
+result_exponentiation = num1 ** num2
+
+print("Addition:", result_addition)
+print("Subtraction:", result_subtraction)
+print("Multiplication:", result_multiplication)
+print("Division:", result_division)
+print("Modulus:", result_modulus)
+print("Floor Division:", result_floor_division)
+print("Exponentiation:", result_exponentiation)
