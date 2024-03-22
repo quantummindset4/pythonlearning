f3f14d58608856a99141628d047030404a3acf18
quantummindset4 <147822552+quantummindset4@users.noreply.github.com>
2024-03-22 15:14:53 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,11 @@
+# Determine the grade based on the exam score range
+exam_score = 85
+
+if exam_score >= 90:
+    print("Grade: A")
+elif exam_score >= 80:
+    print("Grade: B")
+elif exam_score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: D")
