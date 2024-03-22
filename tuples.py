4f8492f7bd143605dece9505a052b74ac76700e8
quantummindset4 <147822552+quantummindset4@users.noreply.github.com>
2024-03-22 15:11:23 +0530

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,9 @@
+# Indexing and slicing
+my_tuple = (1, 2, 3, 4, 5)
+first_element = my_tuple[0]
+last_three_elements = my_tuple[-3:]
+subset = my_tuple[1:4]
+
+print("First Element:", first_element)
+print("Last Three Elements:", last_three_elements)
+print("Subset:", subset)
